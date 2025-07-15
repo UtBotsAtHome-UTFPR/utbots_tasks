010264be53e7e4904f3e1b686bd3f1f68fd76bad
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2025-07-15 17:01:33 -0300

    Find people to stare

--- utbots_tasks/states/basic_face.py
+++ utbots_tasks/states/basic_face.py
@@ -9,6 +9,8 @@ from yasmin_ros.basic_outcomes import SUCCEED, ABORT, CANCEL
 from yasmin_viewer import YasminViewerPub
 from utbots_actions.action import NewFace, Recognition
 
+from utbots_tasks.states.basic_nav import generate_rotate_in_place
+
 from std_srvs.srv import SetBool
 
 from math import pow, sqrt, sin, tan, radians, degrees
@@ -23,6 +25,24 @@ If you need to change between testing just the state or the hierarquical state m
 
 '''
 
+class SavePosition(State):
+    def __init__(self) -> None:
+        super().__init__([SUCCEED, CANCEL])
+
+    def execute(self, blackboard: Blackboard) -> str:
+        
+        key = blackboard["people_yaw"][-1]
+
+        # Lê quaternion aqui
+        quaternion = blackboard["current_pose"]
+
+        for person in blackboard["person_list"]:
+            
+            if person["name"] == key:
+                person["quat"] = quaternion
+
+        return SUCCEED
+
 class IdentifyYAW(State):
     def __init__(self) -> None:
         super().__init__([SUCCEED, CANCEL])
@@ -34,17 +54,15 @@ class IdentifyYAW(State):
         # Read people direction list (if not exist create) if person does not have yaw set it up
         if "people_yaw" not in blackboard:
             blackboard["people_yaw"] = []
-
+        
         people = blackboard["people"]
 
-        width  = 1280
-        height = 1.0
-        theta_max = 78/2
+        width  = 1280 # CHANGE TO 1920
+        theta_max = 78/2 # Logitech cam FOV 
 
         for person in people:
-            if person.id == "Unknown":
+            if person.id == "Unknown" or person.id in blackboard["people_yaw"]:
                 continue
-            print(person.id)
 
             x = int((person.xmin + person.xmax) / 2)
             #y = int((person.ymin + person.ymax) / 2)
@@ -54,56 +72,13 @@ class IdentifyYAW(State):
             theta = radians(theta_max * x / (width/2))
 
             angle = degrees(theta)
+
+            blackboard["people_yaw"].append(person.id)
+            blackboard["rotate"] = angle
+            return SUCCEED
             print(degrees(theta), end = "\n\n\n\n")
-            # Convert spherical radius from mm to meters
-            #rho = distance / 1000.0
 
-            # Assume phi = 0 → y = 0 → sin(phi) = 0 → vertical displacement ignored
-            # So y = 0, and the radius is entirely in the xz-plane
-
-            # Calculate x and z using theta only
-            #x_coord = rho * sin(theta)
-
-        return SUCCEED
-'''
-    def Get3dPointFromDepthPixel(self, pixel, distance):
-        # The height and width are already scaled to 0-1 both by Mediapipe
-        width  = 1.0
-        height = 1.0
-
-        # Centralize the camera reference at (0,0,0)
-        ## (x,y,z) are respectively horizontal, vertical and depth
-        ## Theta is the angle of the point with z axis in the zx plane
-        ## Phi is the angle of the point with z axis in the zy plane
-        ## x_max is the distance of the side border from the camera
-        ## y_max is the distance of the upper border from the camera
-        theta_max = self.camFov_horizontal/2 
-        phi_max = self.camFov_vertical/2
-        x_max = width/2.0
-        y_max = height/2.0
-        x = pixel.x - x_max
-        y = pixel.y - y_max
-
-        # Caculate point theta and phi
-        theta = radians(theta_max * x / x_max)
-        phi = radians(phi_max * y / y_max)
-
-        # Convert the spherical radius rho from Kinect's mm to meter
-        rho = distance/1000
-
-        # Calculate x, y and z
-        y = rho * sin(phi)
-        x = sqrt(pow(rho, 2) - pow(y, 2)) * sin(theta)
-        z = x / tan(theta)
-
-        # Change coordinate scheme
-        ## We calculate with (x,y,z) respectively horizontal, vertical and depth
-        ## For the plot in 3d space, we need to remap the coordinates to (z, -x, -y)
-        point_zxy = Point(z, -x, -y)
-
-        return point_zxy'''
-
-        # utbots_msgs.msg.BoundingBox(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=0, nanosec=0), frame_id=''), probability=0.0, xmin=587, ymin=245, xmax=760, ymax=464, xminn=0.0, yminn=0.0, xmaxn=0.0, ymaxn=0.0, id='Teste', category='Person')
+        return CANCEL
 
 class USBCamOn(ServiceState):
     def __init__(self) -> None:
@@ -397,6 +372,8 @@ def yaw_test():
     # Set up ROS 2 logs
     set_ros_loggers()
 
+    node = rclpy.create_node("rotate_person")
+
     # Create a finite state machine (FSM)
     sm = StateMachine(outcomes=[SUCCEED, CANCEL])
 
@@ -413,8 +390,26 @@ def yaw_test():
         "IDENTIFY_YAW",
         IdentifyYAW(),
         transitions={
-            SUCCEED: "RECOGNITION_SM",
-            CANCEL: CANCEL,
+            SUCCEED: "ROTATE_TO_PERSON",
+            CANCEL: CANCEL, # Girar 45º
+        },
+    )
+
+    sm.add_state(
+        "ROTATE_TO_PERSON",
+        generate_rotate_in_place(node),
+        transitions={
+            SUCCEED: SUCCEED,
+            ABORT: CANCEL
+        },
+    )
+
+    sm.add_state(
+        "SAVE_POSITION",
+        SavePosition(),
+        transitions={
+            SUCCEED: SUCCEED,
+            CANCEL: CANCEL
         },
     )
 
