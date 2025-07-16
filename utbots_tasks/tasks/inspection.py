da18b836380a84549f1c8a3ec03d47c5a392ac44
gustavo-fardo <gustavo.fardo@hotmail.com>
2025-07-16 10:28:25 -0300

    inspection sm

---
+++ utbots_tasks/tasks/inspection.py
@@ -0,0 +1,68 @@
+import rclpy
+#Monkey path (TODO:change)
+import numpy as np
+if not hasattr(np, 'float'):
+    np.float = float
+import yasmin
+from yasmin import Blackboard, StateMachine
+from yasmin_ros import set_ros_loggers
+from yasmin_ros.basic_outcomes import SUCCEED, ABORT, CANCEL
+from yasmin_viewer import YasminViewerPub
+
+from utbots_tasks.states.basic_nav import GoToWaypointState, WaitDoorOpenState, SetInitialPose
+
+def main():
+    yasmin.YASMIN_LOG_INFO("yasmin_action_client")
+    rclpy.init()
+    node = rclpy.create_node("inspection_sm")
+
+    # Set up ROS 2 logs
+    set_ros_loggers()
+
+    # Create a finite state machine (FSM)
+    sm = StateMachine(outcomes=["success", "failed"])
+
+    sm.add_state(
+        "SET_INIT_POSE",
+        SetInitialPose(node, 0.0, 0.0, 0.0),
+        transitions={
+            SUCCEED: "WAIT_DOOR",
+            ABORT: "failed"
+        }
+    )
+
+    sm.add_state(
+        "WAIT_DOOR",
+        WaitDoorOpenState(),
+        transitions={
+            SUCCEED: "GO_TO_WAYPOINT",
+            CANCEL: "WAIT_DOOR",
+            ABORT: "failed"
+        }
+    )
+
+    sm.add_state(
+        "GO_TO_WAYPOINT",
+        GoToWaypointState(),
+        transitions={
+            SUCCEED: "success",
+            ABORT: "failed"
+        },
+        remappings={"waypoint_nametag" : "inspection"}
+    )
+
+    # Publish FSM information
+    YasminViewerPub("YASMIN_ACTION_CLIENT_DEMO", sm)
+
+    blackboard = Blackboard()
+    try:
+        outcome = sm(blackboard)
+        yasmin.YASMIN_LOG_INFO(outcome)
+    except KeyboardInterrupt:
+        if sm.is_running():
+            sm.cancel_state()  # Cancel the state if interrupted
+
+    # Shutdown ROS
+    if rclpy.ok():
+        rclpy.shutdown()
+
